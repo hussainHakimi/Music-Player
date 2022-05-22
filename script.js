3b243bfa50b328b2c84f8a210f1443c11e4d2f86
hussainHakimi <hussainbayan614@gmail.com>
2022-05-22 23:39:55 +0430

    changes added to js file

---
+++ script.js
@@ -0,0 +1,5 @@
+const box = document.querySelector('.box');
+const img_section = document.querySelector('.img-section');
+const details = document.querySelector('.details');
+const music_name = document.querySelect('.music_name');
+const music_artist = document.querySelect('.music_artist');
