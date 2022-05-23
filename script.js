c441a15016bd1cbc8d3a248a58820ca4be9c3d1a
hussainHakimi <hussainbayan614@gmail.com>
2022-05-23 23:24:16 +0430

    first phase is done.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+const body = document.querySelector('body');
+const contianer = document.querySelector('.container');
 const box = document.querySelector('.box');
 const img_section = document.querySelector('.img-section');
 const details = document.querySelector('.details');
-const music_name = document.querySelect('.music_name');
-const music_artist = document.querySelect('.music_artist');
+const music_name = document.querySelector('.music_name');
+const music_artist = document.querySelector('.music_artist');
+const range_control = document.querySelector('.range-control');
+const current_time = document.querySelector('.current_time');
+const duration_time = document.querySelector('.duration_time');
+const btnBack = document.querySelector('.btnBack');
+const play = document.querySelector('.ply_btn')
+const puase = document.querySelector('.paus');
+const btnNext = document.querySelector('.btnNext');
+console.log(btnNext);
