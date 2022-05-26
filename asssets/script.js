04a72f42f384de854af6ffca65da1a4373244b34
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-05-27 00:10:49 +0430

    added the set music

--- asssets/script.js
+++ asssets/script.js
@@ -1,48 +1,47 @@
 
-let currentMusic=0;
 const playBtn=document.querySelector('.ply_btn');
 const backBatn=document.querySelector('.btnBack');
 const nextBtn=document.querySelector('.btnNext');
 const disk=document.querySelector('.disk');
-const music=document.querySelector('#audio');
 const rangeControl=document.querySelector('.range-control');
 const musicName=document.querySelector('.music_name');
 const musicArtist=document.querySelector('.music_artist');
 const currentTime=document.querySelector('.current_time');
-const durationTime=document.querySelector('.duration_time');
+const durationTime=document.querySelector('.duration_time'); 
+const currentMusic = document.createElement('audio');
 
 let i = 0;
-setMusic(i);
 //add the songs
 let songs = [
-{
-    name:'Arash',
-    path:'../music/song1.mp3',
-    artist:'artist1',
-    imgcover:'/img/coverimg1.jpg',
-},
+    {
+        name:'Arash',
+        path:'song1.mp3',
+        artist:'artist1',
+        imgcover:'/img/coverimg1.jpg'
+    },
+    {
+        name:'Arash',
+        path:'/music/song1.mp3',
+        artist:'artist1',
+        imgcover:'/img/cover2.jpg'
+    },
 {
     name:'Arash',
     path:'/music/song1.mp3',
     artist:'artist1',
-    imgcover:'/img/cover2.jpg',
-},
-{
-    name:'Arash',
-    path:'/music/song1.mp3',
-    artist:'artist1',
-    imgcover:'/img/cover2.jpg',
+    imgcover:'/img/cover2.jpg'
 }
 ];
 //set music up
 function setMusic(i){
-    currentMusic = songs[i].path;
+    currentMusic.src = songs[i].path;
     currentMusic.load();
     disk.style.backgroundImage=songs[i].imgcover;
     musicName.textContent=songs[i].name;
     musicArtist.textContent=songs[i].artist;
-
+    
 }
+setMusic(i);
 // const setMusic= (i)=>{
 // }
 
@@ -51,7 +50,6 @@ function setMusic(i){
 
 playBtn.addEventListener('click', ()=>{
     playBtn.classList.toggle('pause');
-    console.log('hello');
     currentMusic.play();
 });
 
